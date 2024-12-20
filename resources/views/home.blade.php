48c11e02183ff9478dd07704f26b29fcef7929ea
kutman <kutmanbekbolotov2003@gmail.com>
2024-12-20 17:15:22 +0600

    homepage added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome to the Home Page</h1>
+
+    <div>
+        <h2>Available Services:</h2>
+        <ul>
+            <li><a href="{{ route('weather.index') }}">Weather Checker</a></li>
+            <li><a href="{{ route('image.get') }}">Image Search</a></li>
+            <li><a href="{{ route('domain.index') }}">Domain Checker</a></li>
+        </ul>
+    </div>
+</body>
+</html>
