38bfd6263291d8dc12b6a6e520fcc20b5a69d784
kutman <kutmanbekbolotov2003@gmail.com>
2024-12-24 16:32:12 +0600

    fixed controller

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -7,34 +7,31 @@ use Illuminate\Support\Facades\Http;
 
 class CurrencyController extends Controller
 {
- public function index(){
-    return view('currency.index');
- }
+    public function index()
+    {
+        return view('currency.index');
+    }
 
- public function indexPost(Request $request){
-     $request->validate([ 'query' => 'required|string']);
+    public function indexPost(Request $request)
+    {
+        $request->validate([
+            'query' => 'required|string',
+        ]);
 
-     $currency = strtoupper($request->input('query'));
+        $currency = strtoupper($request->input('query'));
 
-            
-            $apiUrl = 'https://v6.exchangerate-api.com/v6/e02cdd4905c949ed1f05fcf7/latest/USD';
-            $apiKey = 'e02cdd4905c949ed1f05fcf7'; 
+        $apiUrl = 'https://v6.exchangerate-api.com/v6/' . env('EXCHANGE_RATE_API_KEY') . '/latest/USD';
 
         try {
-            $response = Http::get($apiUrl, [
-                'app_id' => $apiKey,
-                'base' => 'KGS',
-            ]);
 
-           
+            $response = Http::get($apiUrl);
+
             if ($response->successful()) {
                 $data = $response->json();
+                $rate = $data['conversion_rates'][$currency] ?? null;
 
-                $rate = $data['rates'][$currency] ?? null;
-
-                
                 if ($rate) {
-                    return view('currencies.result', [
+                    return view('currency.result', [
                         'currency' => $currency,
                         'rate' => $rate,
                     ]);
@@ -49,7 +46,3 @@ class CurrencyController extends Controller
         }
     }
 }
-
- 
-
-
