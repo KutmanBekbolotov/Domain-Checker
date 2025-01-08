9430d1aa32fe94ac4bc3d2c8fcc58ee59c33eac5
akimovvb <bekmamatakimov@gmail.com>
2025-01-08 17:10:15 +0600

    s

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\MusicController;
 
 use App\Http\Controllers\MapController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\StoreController;
 
 
 Route::get('/', function () {
@@ -41,6 +42,10 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+
+
+Route::get('/store', [StoreController::class, 'store'])->name('store.index');
 
 
   
